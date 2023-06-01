71e3975c0255b873fd38065f7804afc93bd2def4
Pedro <pedrosilva1137@gmail.com>
2023-06-01 21:34:08 +0100

    Update code to add new category and profissional column

--- src/contentScript/helpers.ts
+++ src/contentScript/helpers.ts
@@ -1,43 +1,47 @@
 import {
     ALIMENTACAO_HOTELARIA_KEYWORDS,
     ANIMAIS_KEYWORDS,
-    BUTTONS_MAPPING,
+    CATEGORY_BUTTONS_MAPPING,
     CABELEIREIRO_KEYWORDS,
     EXACT_COMPANIES_MAPPING,
     OUTROS_KEYWORDS,
     REPARACAO_AUTOMOVEL_KEYWORDS,
     SAUDE_KEYWORDS,
-    TRANSPORTES_KEYWORDS
+    TRANSPORTES_KEYWORDS,
+    REVISTAS_JORNAIS_KEYWORDS
 } from "./constants";
 
 const keywordMatches = (company: string, keywords: string[]): boolean => keywords.some((word: string) => company.includes(word))
 
-const similarMappings = (company: string): BUTTONS_MAPPING | undefined => {
-    const companyLowercase = company.toLowerCase()
+const similarMappings = (company: string): CATEGORY_BUTTONS_MAPPING | undefined => {
 
-    if (keywordMatches(companyLowercase, SAUDE_KEYWORDS)) {
-        return BUTTONS_MAPPING.Saude
-    } else if (keywordMatches(companyLowercase, ALIMENTACAO_HOTELARIA_KEYWORDS)) {
-        return BUTTONS_MAPPING["Alimentacao/Hotelaria"]
-    } else if (keywordMatches(companyLowercase, TRANSPORTES_KEYWORDS)) {
-        return BUTTONS_MAPPING.Transportes
-    } else if (keywordMatches(companyLowercase, CABELEIREIRO_KEYWORDS)) {
-        return BUTTONS_MAPPING.Cabeleireiro
-    } else if (keywordMatches(companyLowercase, OUTROS_KEYWORDS)) {
-        return BUTTONS_MAPPING.Outros
-    } else if (keywordMatches(companyLowercase, REPARACAO_AUTOMOVEL_KEYWORDS)) {
-        return BUTTONS_MAPPING["Reparacao Automovel"]
-    } else if (keywordMatches(companyLowercase, ANIMAIS_KEYWORDS)) {
-        return BUTTONS_MAPPING["Animais de Estimacao"]
+    if (keywordMatches(company, SAUDE_KEYWORDS)) {
+        return CATEGORY_BUTTONS_MAPPING.Saude
+    } else if (keywordMatches(company, ALIMENTACAO_HOTELARIA_KEYWORDS)) {
+        return CATEGORY_BUTTONS_MAPPING["Alimentacao/Hotelaria"]
+    } else if (keywordMatches(company, TRANSPORTES_KEYWORDS)) {
+        return CATEGORY_BUTTONS_MAPPING.Transportes
+    } else if (keywordMatches(company, CABELEIREIRO_KEYWORDS)) {
+        return CATEGORY_BUTTONS_MAPPING.Cabeleireiro
+    } else if (keywordMatches(company, OUTROS_KEYWORDS)) {
+        return CATEGORY_BUTTONS_MAPPING.Outros
+    } else if (keywordMatches(company, REPARACAO_AUTOMOVEL_KEYWORDS)) {
+        return CATEGORY_BUTTONS_MAPPING["Reparacao Automovel"]
+    } else if (keywordMatches(company, ANIMAIS_KEYWORDS)) {
+        return CATEGORY_BUTTONS_MAPPING["Animais de Estimacao"]
+    } else if (keywordMatches(company, REVISTAS_JORNAIS_KEYWORDS)) {
+        return CATEGORY_BUTTONS_MAPPING["Jornais e Revista"]
     }
 }
 
 export const mapCompanyToButton = (company: string): number => {
-    const found = EXACT_COMPANIES_MAPPING.find((item) => item.company === company)
+    const companyLowercase = company.toLowerCase()
+
+    const found = EXACT_COMPANIES_MAPPING.find((item) => item.company === companyLowercase)
 
     if (found) {
         return found.button
     }
 
-    return similarMappings(company)
+    return similarMappings(companyLowercase)
 }
--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,4 +1,4 @@
-import { BUTTONS_MAPPING } from "./constants";
+import { CATEGORY_BUTTONS_MAPPING, COLUMNS_MAPPING, PROFESSIONAL_BUTTONS_MAPPING } from "./constants";
 import { mapCompanyToButton } from "./helpers";
 
 const table: Element = document.querySelector("#listaPendencias > tbody")
@@ -7,12 +7,20 @@ if (table?.childElementCount > 0) {
     for (let item of table.children) {
         const company: string = item.firstChild.textContent.trim()
 
-        const buttonMapped: BUTTONS_MAPPING | undefined = mapCompanyToButton(company)
+        // Click on the expected category
+        const buttonMapped: CATEGORY_BUTTONS_MAPPING | undefined = mapCompanyToButton(company)
         if (buttonMapped) {
-            // @ts-ignore
-            const buttons = item.lastChild!.children[0];
+            const categoryButtons = item.children[COLUMNS_MAPPING.CATEGORY_BUTTONS_POSITION]!.children[0];
 
-            buttons.children[buttonMapped].click()
+            // @ts-ignore
+            categoryButtons.children[buttonMapped]!.click()
         }
+
+        // Set expense as not professional
+        const professionalButtons = item.children[COLUMNS_MAPPING.PROFESSIONAL_BUTTONS_POSITION].children[0]
+        const naoButton = professionalButtons.children[PROFESSIONAL_BUTTONS_MAPPING.NAO]
+        
+        // @ts-ignore
+        naoButton.click()
     }
 }
