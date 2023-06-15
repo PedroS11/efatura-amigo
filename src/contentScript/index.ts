1a596f79df5c6876acd644becb67a73f34e46752
Pedro Oliveira da Silva <9307951+PedroS11@users.noreply.github.com>
2023-06-15 14:08:08 +0100

    Remove Professional Expense selector section as it no longer exists

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,4 +1,4 @@
-import { CATEGORY_BUTTONS_MAPPING, COLUMNS_MAPPING, PROFESSIONAL_BUTTONS_MAPPING } from "./constants";
+import { CATEGORY_BUTTONS_MAPPING, COLUMNS_MAPPING } from "./constants";
 import { mapCompanyToButton } from "./helpers";
 
 const table: Element = document.querySelector("#listaPendencias > tbody")
@@ -15,12 +15,5 @@ if (table?.childElementCount > 0) {
             // @ts-ignore
             categoryButtons.children[buttonMapped]!.click()
         }
-
-        // Set expense as not professional
-        const professionalButtons = item.children[COLUMNS_MAPPING.PROFESSIONAL_BUTTONS_POSITION].children[0]
-        const naoButton = professionalButtons.children[PROFESSIONAL_BUTTONS_MAPPING.NAO]
-        
-        // @ts-ignore
-        naoButton.click()
     }
 }
